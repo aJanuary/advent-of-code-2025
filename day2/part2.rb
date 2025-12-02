ca5286ac9331969a0603063a4546f824cf24d205
Andrew January <andrewjanuary@gmail.com>
2025-12-02 09:23:41 +0000

    Day 2: Use substring instead of arrays for part 2
    Doing the conversion to an array to use each_slice ends up being
    expensive. Manually looping over the substrings ends up being cheaper,
    and not too difficult to read.
    
    This also adds the optimizations of only checking substring lenghts up
    to half the string, and that divide the string evenly.

--- day2/part2.rb
+++ day2/part2.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
 
+def has_repeating_substring(str)
+  (1..str.length / 2).filter {|i| str.length % i == 0}.any? do |period|
+    pattern = str[0, period]
+
+    (period...str.length).step(period).all? do |start|
+      str[start, period] == pattern
+    end
+  end
+end
+
 ranges = STDIN.read.chomp.split(',').map do |range_str|
   Range.new(*range_str.split('-').map(&:to_i))
 end
 
 invalid = ranges.flat_map do |range|
   range.filter do |i|
-    i_chars = i.to_s.chars
-    (1...i_chars.length).any? do |reps|
-      i_chars.each_slice(reps).uniq.length == 1
-    end
+    has_repeating_substring(i.to_s)
   end
 end
 
